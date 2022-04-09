1f3b73d4f7240545ba61c6efd0cb4ff1c4e5d5b8
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-04-09 07:03:06 +0600

    on auth state change

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {signInWithGoogle} = useFirebase();
     return (
         <div>
             <h3>Please Login</h3>
+            <div style={{margin: '20px'}}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+                </div>
+            <form>
+                <input type="email" placeholder='Your Email' /><br />
+                <input type="password" placeholder='Your password' /><br />
+                <input type="submit" value="login" />
+            </form>
         </div>
     );
 };
---
+++ src/components/Register/Register.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h3>Please Register Now</h3>
+            <form>
+                <input type="text" placeholder='Your Name' /> <br />
+                <input type="email" placeholder='Your Email' /><br />
+                <input type="password" placeholder='Your password' /><br />
+                <input type="submit" value="Register" />
+            </form>
+        </div>
+    );
+};
+
+export default Register;
