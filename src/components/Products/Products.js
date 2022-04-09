fc67b7ed70e32374b0300261d3fcb6426fb74e4d
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-04-09 09:00:34 +0600

    require auth set

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,18 +1,18 @@
 import { getAuth } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import app from '../../../firebase.init';
+import app from '../../firebase.init';
 
 const auth = getAuth(app);
 
-const Home = () => {
+const Products = () => {
     const [user] = useAuthState(auth);
     return (
         <div>
-            <h2>This is home</h2>
-            <p>Current user is: {user? user.displayName : 'No body'}</p>
+            <h2>knock knock who is there!!!</h2>
+            <h5>{user? user.displayName : 'Voooooot'}</h5>
         </div>
     );
 };
 
-export default Home;
+export default Products;
